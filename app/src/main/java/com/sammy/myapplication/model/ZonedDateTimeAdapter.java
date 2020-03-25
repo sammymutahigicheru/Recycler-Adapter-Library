b910c86df3d9dfdc0887656410f385e991463270
sammymutahi12@gmail.com <sammymutahi12@gmail.com>
2020-03-25 20:53:05 +0300

    add models and their adapters

---
+++ app/src/main/java/com/sammy/myapplication/model/ZonedDateTimeAdapter.java
@@ -0,0 +1,23 @@
+package com.sammy.myapplication.model;
+
+
+
+import androidx.annotation.Nullable;
+
+import com.squareup.moshi.FromJson;
+import com.squareup.moshi.ToJson;
+
+import org.threeten.bp.ZonedDateTime;
+
+public class ZonedDateTimeAdapter {
+
+    @FromJson
+    ZonedDateTime fromJson(String json) {
+        return ZonedDateTime.parse(json);
+    }
+
+    @ToJson
+    String toJson(@Nullable ZonedDateTime value) {
+        return value != null ? value.toString() : null;
+    }
+}
